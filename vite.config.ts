fc8403bd00123e4b88175c981c7229d52d764d1d
Aushev Nikita <mr_uber@mail.ru>
2023-03-27 09:12:24 +0500

    Added fonts alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
     modules: {
       localsConvention: 'camelCase',
       generateScopedName: '[name]_[local]_[hash:base64:5]',
+    },
+  },
+  resolve: {
+    alias: {
+      $fonts: resolve('./public/fonts'),
     },
   },
 });
